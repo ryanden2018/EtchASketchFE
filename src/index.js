8cea1381b1096304f24a91be68ab5488ebb8a0af
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-28 11:51:40 -0500

    update display feature

--- src/index.js
+++ src/index.js
@@ -12,14 +12,18 @@ class Sketch {
 
   static setColor() { return "black"; }
 
-  static pxw() { return 20; }
+  static pxw() { return 2; }
 
-  static pxh() { return 20; }
+  static pxh() { return 2; }
+
+  static pixelStyleString(i,j,color) {
+    return `position:absolute;width:${Sketch.pxw()}px;height:${Sketch.pxh()}px;top:${i*Sketch.pxh()}px;left:${j*Sketch.pxw()}px;background:${color}`;
+  }
 
   // make a string of zeros corresponding to an image of dimension (width,height)
   static zeroData(width,height) {
     let result = "";
-    for(i=0; i<width*height; i++) {
+    for(let i=0; i<width*height; i++) {
       result += "0";
     }
     return result;
@@ -48,21 +52,22 @@ class Sketch {
     return str;
   }
 
+
+
   // return a div containing representation of the image data
   // this should only be called once, see update()
   render() {
     this.div = document.createElement("div");
+    this.div.style = `position:absolute;width:${this.width*Sketch.pxw()}px;height:${this.height*Sketch.pxh()}px;background:${Sketch.unsetColor()};`;
 
     for( let i = 0; i < this.height; i++) {
       for( let j = 0; j < this.width; j++) {
-        let pxDiv = document.createElement("div");
-        pxDiv.id = ("" + i) + (" " + j);
-        let color = Sketch.unsetColor();
         if(this.image[i][j] === 1) {
-          color = Sketch.setColor();
+          let pxDiv = document.createElement("div");
+          pxDiv.id = `p${i}_${j}`;
+          pxDiv.style = Sketch.pixelStyleString(i,j,Sketch.setColor());
+          this.div.append(pxDiv);
         }
-        pxDiv.style = "position:absolute;width:"+Sketch.pxw()+"px;height:"+Sketch.pxh()+"px;top:"+(i*Sketch.pxh())+"px;left:"+(j*Sketch.pxw())+"px;background:"+color;
-        this.div.append(pxDiv);
       }
     }
 
@@ -70,8 +75,24 @@ class Sketch {
   }
 
   // update this.div to reflect altered internal state
-  // warning: 
   update() {
+    for(let i=0; i<this.height; i++) {
+      for(let j=0; j<this.width; j++) {
+        let pxDiv = document.querySelector(`#p${i}_${j}`);
+        if(pxDiv && (this.image[i][j] === 0)) {
+          pxDiv.style = `display:none;`;
+        } else if(pxDiv && (this.image[i][j] === 1)) {
+          pxDiv.style = Sketch.pixelStyleString(i,j,Sketch.setColor());
+        } else { // pxDiv is null
+          if(this.image[i][j] === 1) {
+            pxDiv = document.createElement("div");
+            pxDiv.id = `p${i}_${j}`;
+            pxDiv.style = Sketch.pixelStyleString(i,j,Sketch.setColor());
+            this.div.append(pxDiv);
+          }
+        }
+      }
+    }
   }
 }
 
@@ -81,6 +102,35 @@ class Sketch {
 // document load callback
 document.addEventListener("DOMContentLoaded", e=>{
   // Testing Code
-  let b = new Sketch( {width:5,height:5,data:"0000000111101011111100000"});
+  let width = 412;
+  let height = 277;
+  let b = new Sketch( {width:width,height:height,data:Sketch.zeroData(width,height)});
   document.body.append(b.render());
+  for(let i=0; i<height; i++) {
+    for(let j=0; j<width; j++) {
+      if(i === j) {
+        b.image[i][j] = 1;
+      }
+    }
+  }
+  b.update();
+  for(let i=0; i<height; i++) {
+    for(let j=0; j<width; j++) {
+      if((i === j) && (i > 100)) {
+        b.image[i][j] = 0;
+      }
+      if(i === 200-j) {
+        b.image[i][j] = 1;
+      }
+    }
+  }
+  b.update();
+  for(let i=0; i<height; i++) {
+    for(let j=0; j<width; j++) {
+      if((i === j) && (i > 150)) {
+        b.image[i][j] = 1;
+      }
+    }
+  }
+  b.update();
 });
